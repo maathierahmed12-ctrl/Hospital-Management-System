9205ca3d152eaa764a5be87cdd090fcf0fad6e67
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-05-10 16:16:01 +0400

    MedicalRecord

---
+++ src/Entity/MedicalRecord.java
@@ -0,0 +1,100 @@
+package Entity;
+
+import java.util.List;
+import java.time.LocalDate;
+
+public class MedicalRecord {
+
+    private String recordId ;
+    private String patientId ;
+    private String doctorId ;
+    private LocalDate visitDate ;
+    private String diagnosis ;
+    private String prescription ;
+    private String testResults ;
+    private String notes;
+
+    public MedicalRecord(String recordId ,String patientId , String doctorId, LocalDate visitDate , String diagnosis , String prescription , String testResults , String notes){
+
+        this.recordId=recordId;
+        this.patientId=patientId;
+        this.doctorId=doctorId;
+        this.visitDate=visitDate;
+        this.diagnosis=diagnosis;
+        this.prescription=prescription;
+        this.testResults=testResults;
+        this.notes=notes;
+
+    }
+
+    public String getRecordId(){
+        return recordId;
+    }
+
+    public void setRecordId(String recordId){
+        this.recordId=recordId;
+    }
+
+    public String getpatientId(){
+        return patientId;
+    }
+    public void setPatientId(String patientId){
+
+        this.patientId=patientId;
+    }
+    public String getDoctorId(){
+        return doctorId;
+    }
+    public void setDoctorId(String doctorId){
+
+        this.doctorId=doctorId;
+    }
+
+    public LocalDate getVisitDate(){
+        return visitDate;
+    }
+    public void  setvisitDate(LocalDate visitDate){
+
+        this.visitDate=visitDate;
+    }
+
+    public String getDiagnosis(){
+
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis){
+        this.diagnosis=diagnosis;
+    }
+
+    public String getprescription(){
+        return prescription;
+    }
+
+    public String gettestResults(){
+        return testResults;
+    }
+    public void setTestResults(String testResults){
+
+        this.testResults=testResults;
+    }
+
+    public String getNotes(){
+        return notes;
+    }
+    public void setNotes(String Notes){
+        this.notes=notes;
+    }
+    public void displayInfo(){
+
+        System.out.println("recordId" + recordId);
+        System.out.println("patientId" + patientId);
+        System.out.println("doctorId" + doctorId);
+        System.out.println("visitDate" + visitDate);
+        System.out.println("diagnosis" + diagnosis);
+        System.out.println("prescription" + prescription);
+        System.out.println("testResults" + testResults);
+        System.out.println("notes" + notes);
+
+    }
+}
