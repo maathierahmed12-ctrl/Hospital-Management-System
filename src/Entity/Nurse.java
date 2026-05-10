1c20eaae9316c7e4d32b157f0715588862e04320
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-05-10 15:47:31 +0400

    Person

---
+++ src/Entity/Nurse.java
@@ -0,0 +1,93 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Nurse extends Person {
+
+    private String nurseId;
+    private String departmentId;
+    private String shift;
+    private String qualification;
+    private List<String> assignedPatients;
+
+
+    public Nurse(String id, String firstname, String lastname, LocalDate dateofBrith, String gender, String phoneNumber, String email, String address) {
+        super(id, firstname, lastname, dateofBrith, gender, phoneNumber, email, address, String nurseId, String departmentId, String shift, String qualification, List < String > assignedPatients);
+
+        this.nurseId = nurseId;
+        this.departmentId = departmentId;
+        this.shift = shift;
+        this.qualification = qualification;
+        this.assignedPatients = assignedPatients;
+
+    }
+
+    public String getnurseId() {
+        return nurseId;
+    }
+
+    public void setnurseId(String nurseId) {
+        this.nurseId = nurseId;
+    }
+
+    public String getdepartmentId() {
+        return departmentId;
+    }
+
+    public void setdepartmentId(String departmentId) {
+
+        this.departmentId = departmentId;
+    }
+
+    public String getshift() {
+
+        return shift;
+    }
+
+    public void setShift(String MorningEveningNigh) {
+
+        this.shift = shift;
+    }
+
+    public String getQualification() {
+
+        return qualification;
+    }
+
+    public List<String> getassignedPatients() {
+
+        return assignedPatients;
+    }
+
+    public List<String> setassignedPatients(List<String> assignedPatients) {
+
+        this.assignedPatients = assignedPatients;
+    }
+
+    @Override
+
+    public void displayInfo(){
+
+        super.displayIfo();
+
+        System.out.println("nurseId" + nurseId);
+        System.out.println("departmentId" + departmentId);
+        System.out.println("shift" + shift);
+        System.out.println("qualification" + qualification);
+        System.out.println("assignedPatients" + assignedPatients);
+
+    }
+
+    public void addpatientassignment( String patientassignmen){
+
+        if (assignedPatients == null){
+
+            assignedPatients = new ArrayList<>();
+
+        }
+        assignedPatients.add(patientassignmen);
+    }
+
+}
+
