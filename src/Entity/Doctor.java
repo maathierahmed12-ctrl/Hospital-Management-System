a5b33bd11eafc4d874f7e9f782680ca3cdc35df0
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-05-10 15:11:31 +0400

    Person

---
+++ src/Entity/Doctor.java
@@ -0,0 +1,163 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Doctor extends Person {
+
+    private String doctorId;
+    private String specialization;
+    private String qualification;
+    private int experienceYears;
+    private String departmentId;
+    private double consultationFee;
+    private List<String> availableSlots;
+    private List<String> assignedPatients;
+
+
+    public Doctor(String id, String firstname, String lastname,
+                  LocalDate dateofBrith, String gender,
+                  String phoneNumber, String email, String address, String doctorId, String specialization, String qualification,
+                  int experienceYears, String departmentId, double consultationFee, List<String> availableSlots, List<String> assignedPatients) {
+
+        super(id, firstname, lastname, dateofBrith, gender, phoneNumber, email, address);
+
+        this.doctorId = doctorId;
+        this.specialization = specialization;
+        this.qualification = qualification;
+        this.experienceYears = experienceYears;
+        this.departmentId = departmentId;
+        this.consultationFee = consultationFee;
+        this.availableSlots = availableSlots;
+        this.assignedPatients = assignedPatients;
+    }
+
+    public String getDoctorId() {
+
+        return doctorId;
+
+    }
+
+    public void setdoctorId(String doctorId) {
+
+        this.doctorId = doctorId;
+    }
+
+    public String getSpecialization() {
+
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+
+        this.specialization = specialization;
+    }
+
+    public String getqualification() {
+
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+
+        this.qualification = qualification;
+    }
+
+    public int getexperienceYears() {
+
+        return experienceYears;
+    }
+
+    public void setexperienceYears(int experienceYears) {
+
+        this.experienceYears = experienceYears;
+    }
+
+    public String getdepartmentId() {
+
+        return departmentId;
+    }
+
+    public void setdepartmentId(String departmentId) {
+
+        this.departmentId = departmentId;
+    }
+
+    public double getconsultationFee() {
+
+        return consultationFee;
+    }
+
+    public void setConsultationFee(double consultationFee) {
+
+        this.consultationFee = consultationFee;
+    }
+
+    public List<String> getAvailableSlots() {
+
+        return availableSlots;
+    }
+
+    public void setAvailableSlots(List<String> availableSlots) {
+
+        this.availableSlots = availableSlots;
+    }
+
+    public List<String> getassignedPatients() {
+
+        return assignedPatients;
+    }
+
+    public void setassignedPatients(List<String> assignedPatients) {
+
+        this.assignedPatients = assignedPatients;
+    }
+
+    @Override
+
+    public void displayInfo() {
+
+        super.displayIfo();
+        System.out.println("doctorId" + doctorId);
+        System.out.println("specialization" + specialization);
+        System.out.println("qualification" + qualification);
+        System.out.println("experienceYears" + experienceYears);
+        System.out.println("departmentId" + departmentId);
+        System.out.println("consultationFee" + consultationFee);
+        System.out.println("availableSlots" + availableSlots);
+        System.out.println("assignedPatients" + assignedPatients);
+
+    }
+
+    public void assignPatient(String Patient) {
+
+        if (assignedPatients == null) {
+
+            assignedPatients = new ArrayList<>();
+        }
+
+    }
+
+    public void removePatient(String Patient) {
+
+        if (assignedPatients != null){
+
+            assignedPatients.remove(Patient);
+        }
+
+    }
+
+    public void updateAvailability(String Slot) {
+
+        if (availableSlots == null) {
+
+            availableSlots = new ArrayList<>();
+
+            }
+          availableSlots.add(Slot);
+        }
+    }
+
+
+
+
---
+++ src/Entity/Person.java
@@ -0,0 +1,156 @@
+package Entity;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Person {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+    private LocalDate dateofBrith;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public Person(String id, String firstname, String lastname, LocalDate dateofBrith, String gender, String phoneNumber, String email, String address) {
+
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateofBrith = dateofBrith;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+
+        this.id = id;
+    }
+
+    public String getFirstname() {
+
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+
+        this.lastname = lastname;
+    }
+
+    public LocalDate getdateofBrith() {
+
+        return dateofBrith;
+    }
+
+    public void setDateofBrith(LocalDate dateofBrith) {
+
+        this.dateofBrith = dateofBrith;
+    }
+
+    public String getgender() {
+
+        return gender;
+    }
+
+    public void setgender(String gender) {
+
+        this.gender = gender;
+    }
+
+    public String getphoneNumber() {
+
+        return phoneNumber;
+    }
+
+    public void setphoneNumber(String phoneNumber) {
+
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getemail() {
+
+        return email;
+    }
+
+    public void setemail(String email) {
+
+        this.email = email;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public void setaddress(String address) {
+
+        this.address = address;
+    }
+
+    public void displayIfo() {
+
+        System.out.println("id : " + id);
+        System.out.println("firstName : " + firstname);
+        System.out.println("lastName : " + lastname);
+        System.out.println("dateOfBirth : " + dateofBrith);
+        System.out.println("gender : " + gender);
+        System.out.println("phoneNumber : " + phoneNumber);
+        System.out.println("email : " + email);
+        System.out.println("address : " + address);
+    }
+
+    @Override
+    public String toString() {
+
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstname + '\'' +
+                ", lastName='" + lastname + '\'' +
+                ", dateOfBirth=" + dateofBrith +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object m) {
+
+        if (this == m) {
+            return true;
+        }
+
+        if (!(m instanceof Person)) {
+            return false;
+        }
+
+        Person person = (Person) m;
+
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
